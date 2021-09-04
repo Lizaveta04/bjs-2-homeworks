0416ae3fae05d0c6455f07042410b0481bf76cf6
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-09-04 22:00:47 +0300

    Initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = true;
 
-  // Ваш код
+  if (arr1.every((item) => item) === arr2.every((item) => item)){
+  	result;
+  } else {
+  	result = false;
+  }
 
-  return result; // boolean
+  return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  
+  let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item*10);
 
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr; 
 }
