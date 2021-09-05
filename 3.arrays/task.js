f90e894f9c3fc6af60ef3cc4702e701c810d4154
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-09-05 13:50:14 +0300

    Update

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,18 +1,18 @@
 function compareArrays(arr1, arr2) {
-  let result = true;
+  //let result;
 
-  if (arr1.every((item) => item) === arr2.every((item) => item)){
-  	result;
+  if (arr1.length === arr2.length && arr1.every(index => arr1[index] === arr2[index])) {
+    return true;
   } else {
-  	result = false;
+    return false;
   }
 
-  return result;
+  //return result;
+
 }
 
 function advancedFilter(arr) {
   
-  let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item*10);
+  return arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item*10);
 
-  return resultArr; 
 }
