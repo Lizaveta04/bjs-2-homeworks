e1090e26ab9e6db74f42c961db95c1a6329f8495
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-09-12 16:40:38 +0300

    Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+//const student1 = new Student("Tony", "male", 37);
+//const student2 = new Student("Buzz", "female", 35);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
+  //student1.subject = "Algebra";
+  //student2.subject = "Geometry";
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  let marks = [];
+  if(this.marks === undefined) {
+  	marks.push(mark[0]);
+  } else {
+  	marks.push(mark[1]);
+  	marks.push(mark[2]);
+  	marks.push(mark[3]);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (let i = 0; i < marks.length; i++) {
+  	sum += marks[i];
+  }
+  let n = marks.length;
+  let avg = sum / n;
+  this.average = avg;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete Student.subject;
+  delete Student.marks;
+  this.excluded = reason;
+}
+
