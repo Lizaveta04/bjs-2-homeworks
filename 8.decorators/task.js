7d7240cc05ea18dd054901ce77dd83993a5609e7
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-10-03 12:01:28 +0300

    Update

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,23 +1,27 @@
 function cachingDecoratorNew(func) {
-  let cache = {};
+  let cache = [
+    //{ hash: "10,20,30", value: 60 },
+    //{ hash: "2,2,2", value: 6 },
+  ];
   function wrapper(...rest) {
-    const hash = (args) => args.join(','); 
-    let key = hash(rest);
-    if (key in cache) {
-      console.log("Из кэша: " + cache[key]);
-      return "Из кэша: " + cache[key];
-    } 
-    if (Object.keys(cache).lenght > 5) {
-      //?? 
+    const hash = (args) => args.join(','); // получаем правильный хэш
+    let idx = cache.findIndex((item) => item.hash === hash(rest)); // ищем элемент, хэш которого равен нашему хэшу
+    if (idx !== -1) { // если элемент не найден
+      console.log("Из кэша: " + cache[hash(rest)]); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+      return "Из кэша: " + cache[hash(rest)];
     }
-    let result = func(...rest);
-    cache[key] = result;
+    let result = func(...rest); // в кэше результата нет - придётся считать
+    for (let i = 0; i < cache.length; i++) {
+      cache.push(cache[i]); // добавляем элемент с правильной структурой
+    }
+    if (cache.length > 5) {
+        cache.shift(); // если слишком много элементов в кэше надо удалить самый старый (первый)
+    }
     console.log("Вычисляем: " + result);
-    return "Вычисляем: " + result;
+    return "Вычисляем: " + result;  
   }
   return wrapper;
 }
-
 
 function debounceDecoratorNew(func) {
   // Ваш код
