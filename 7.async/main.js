a6fe6167370acc4e347ca27032008e6add4f3f96
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-10-14 14:21:21 +0300

    Function testCase

--- 7.async/main.js
+++ 7.async/main.js
@@ -2,18 +2,16 @@
 function testCase() {
 	let phoneAlarm;
 	phoneAlarm = new AlarmClock();
-	phoneAlarm.addClock('09:00', () => console.log('Пора вставать!'), 1);
-	phoneAlarm.addClock('09:01', () => {
+	phoneAlarm.addClock('14:18', () => console.log('Пора вставать!'), 1);
+	phoneAlarm.addClock('14:19', () => {
 		console.log('Давай, вставай уже!'); 
 		phoneAlarm.removeClock(2);
 	}, 2);
-	phoneAlarm.addClock('09:01', () => console.log('Иди умываться!'));
-	phoneAlarm.addClock('09:02', () => {
+	phoneAlarm.addClock('14:20', () => {
 		console.log('Вставай, а то проспишь!'); 
 		phoneAlarm.clearAlarms();
 		phoneAlarm.printAlarms();
 	}, 3);
-	phoneAlarm.addClock('09:05', () => console.log('Вставай, а то проспишь!'), 1);
 	phoneAlarm.printAlarms();
 	phoneAlarm.start();
 }
